93b98e8ebd9c02de861381d65ca3625b7cda811e
her0elt <hermann.owren.elton@hotmail.com>
2021-08-27 14:11:17 +0200

    done

--- exercise 1/scripts/3d-regression.py
+++ exercise 1/scripts/3d-regression.py
@@ -9,7 +9,7 @@ train_x = torch.tensor(train.to_numpy(), dtype=torch.float).reshape(-1,2)
 train_y = torch.tensor(train_y.to_numpy(), dtype=torch.float).reshape(-1,1)
 
 
-class NonLinearRegressionModel:
+class LinearRegressionModel:
     def __init__(self):
         self.W = torch.rand((2,1), requires_grad=True)
         self.b = torch.rand((1,1), requires_grad=True)
@@ -22,10 +22,12 @@ class NonLinearRegressionModel:
         return x
 
 
-model = NonLinearRegressionModel()
+model = LinearRegressionModel()
 
-optimizer = torch.optim.SGD([model.b, model.W], lr=0.00000075)
-for epoch in range(10000):
+
+
+optimizer = torch.optim.SGD([model.b, model.W], lr=0.000050)
+for epoch in range(600000):
     model.loss(train_x, train_y).backward() 
     optimizer.step() 
     optimizer.zero_grad()  
@@ -40,10 +42,8 @@ yt =train_x.t()[1]
 fig = plt.figure('Linear regression 3d')
 ax = fig.add_subplot(projection='3d', title="Model for predicting days lived by weight and length")
 # Plot
-x = torch.linspace(int(torch.min(xt).item()), int(torch.max(xt).item()), 1000) 
-z = torch.linspace(int(torch.min(yt).item()), int(torch.max(xt).item()), 1000) 
-ax.scatter(x.numpy(),  train_y.numpy(), z.numpy(),label='$(x^{(i)},y^{(i)}, z^{(i)})$')
-ax.scatter(x.numpy(), model.f(train_x).detach().numpy(),z.numpy() , label='$\\hat y = f(x) = xW+b$', color="orange")
+ax.scatter(xt.numpy(),  yt.numpy(), train_y.numpy(),label='$(x^{(i)},y^{(i)}, z^{(i)})$')
+ax.scatter(xt.numpy(),yt.numpy() ,model.f(train_x).detach().numpy() , label='$\\hat y = f(x) = xW+b$', color="orange")
 ax.legend()
 plt.show()
 
--- exercise 1/scripts/sigmoid-regression.py
+++ exercise 1/scripts/sigmoid-regression.py
@@ -1,3 +1,6 @@
+# To add a new cell, type '# %%'
+# To add a new markdown cell, type '# %% [markdown]'
+# %%
 import torch
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -8,12 +11,15 @@ train_y = train.pop('head circumference')
 train_x = torch.tensor(train.to_numpy(), dtype=torch.float)
 train_y = torch.tensor(train_y.to_numpy(), dtype=torch.float).reshape(-1,1)
 
+
+# %%
+
 class NonLinearRegressionModel:
     def __init__(self, max):
         self.max = max
         self.W = torch.tensor([[0.0]], requires_grad=True)
         self.b = torch.tensor([[0.0]], requires_grad=True)
-
+        
     def f(self, x):
         return 20 * torch.sigmoid((x @ self.W + self.b)) + 31
 
@@ -21,9 +27,10 @@ class NonLinearRegressionModel:
         return torch.nn.functional.mse_loss(self.f(x), y)
 
 
+# %%
 model = NonLinearRegressionModel(train.shape[0])
 
-optimizer = torch.optim.SGD([model.b, model.W], 0.000001)
+optimizer = torch.optim.SGD([model.W,model.b], 0.000001)
 for epoch in range(100000):
     model.loss(train_x, train_y).backward() 
     optimizer.step() 
@@ -32,15 +39,19 @@ for epoch in range(100000):
 
 print("W = %s, b = %s, loss = %s" %(model.W, model.b, model.loss(train_x, train_y)))
 
+
+# %%
 plt.figure('Nonlinear regression 2d')
 plt.title('Predict head circumference based on age')
 plt.xlabel('x')
 plt.ylabel('y')
 plt.scatter(train_x, train_y)
 x = torch.arange(torch.min(train_x), torch.max(train_x), 1.0).reshape(-1, 1)
-y = model.f(x).detach().cpu()
-plt.plot(x.cpu(), y, color='orange',
+y = model.f(x).detach()
+plt.plot(x, y, color='orange',
          label='$f(x) = 20\sigma(xW + b) + 31$ \n$\sigma(z) = \dfrac{1}{1+e^{-z}}$')
 
 plt.legend()
 plt.show()
+
+
